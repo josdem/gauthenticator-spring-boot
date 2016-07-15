6163d2a0b3e6c85016403e6348cef5d621ab8c87
josdem <joseluis.delacruz@gmail.com>
2016-07-14 21:49:16 -0500

    Changing to logger a println

--- src/main/groovy/com/jos/dem/authenticator/advice/AfterThrowingAdvice.groovy
+++ src/main/groovy/com/jos/dem/authenticator/advice/AfterThrowingAdvice.groovy
@@ -3,6 +3,8 @@ package com.jos.dem.authenticator.advice
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.AfterThrowing
 import org.springframework.stereotype.Component
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 import com.jos.dem.authenticator.exception.BusinessException
 
@@ -10,9 +12,11 @@ import com.jos.dem.authenticator.exception.BusinessException
 @Component
 class AfterThrowingAdvice {
 
+  Logger logger = LoggerFactory.getLogger(this.class)
+
   @AfterThrowing(pointcut = "execution(* com.jos.dem.authenticator.service..**.*(..))", throwing = "ex")
   void doRecoveryActions(RuntimeException ex){
-    println "Wrapping exception ${ex}"
+    logger.info "Wrapping exception ${ex}"
     throw new BusinessException(ex.getMessage(), ex)
   }
 
